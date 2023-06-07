27e52f33ea4c6e04304fc7619f9f9f372b71d237
Joele-E <esposito.joelebiz@gmail.com>
2023-06-07 18:35:42 +0200

    added object joele

--- team.js
+++ team.js
@@ -1,1 +1,8 @@
 let students = [];
+
+let joele = {'name' : 'Joele', 'surname' : 'Esposito', 'age' : '21', 'city' : 'Massa', 'hobby' : '', 'favoriteFood' : '', 'favoriteVideoGame' : 'Ocarina of time', 'favoriteFilm' : 'Taxi driver', 'favoriteBook' : '', 'petName' : '' }
+
+students.push(joele);
+
+
+console.log(students);

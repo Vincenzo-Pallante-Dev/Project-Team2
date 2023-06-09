6035fd75138ed902e8b18105283170df914f9c4e
Lorena <lorenacus98@gmail.com>
2023-06-09 11:21:43 +0200

    New Student Added

--- team.js
+++ team.js
@@ -1,1 +1,4 @@
-let students = [];
+let students = [
+    student1 = {name: "Lorena", surname: "Cusenza", age: 25, city: "Trapani", hobby: "Cucinare", favoriteFood: "Pizza", favoriteVideoGame: "Super Mario", favoriteFilm: "Sniper", favoriteBook: "Resto qui", petName: "Milka"},
+];
+

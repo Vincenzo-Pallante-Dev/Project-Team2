27a58456d556c2fb8dc47b63cff1ddd17c142684
Joele-E <esposito.joelebiz@gmail.com>
2023-06-09 14:40:04 +0200

    updated hobby

--- team.js
+++ team.js
@@ -1,6 +1,6 @@
 let students = [];
 
-let joele = {'name' : 'Joele', 'surname' : 'Esposito', 'age' : '21', 'city' : 'Massa', 'hobby' : '', 'favoriteFood' : '', 'favoriteVideoGame' : 'Ocarina of time', 'favoriteFilm' : 'Taxi driver', 'favoriteBook' : '', 'petName' : '' }
+let joele = {'name' : 'Joele', 'surname' : 'Esposito', 'age' : '21', 'city' : 'Massa', 'hobby' : 'Guitar playing', 'favoriteFood' : '', 'favoriteVideoGame' : 'Ocarina of time', 'favoriteFilm' : 'Taxi driver', 'favoriteBook' : '', 'petName' : '' }
 
 students.push(joele);
 

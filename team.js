d5767aeffcd1227fe8ac752441eda5c65d2647eb
Vincenzo-Pallante-Dev <vincenzopallante0@gmail.com>
2023-06-10 17:02:09 +0200

    modified2 team.js

--- team.js
+++ team.js
@@ -1,12 +1,12 @@
 let students = [{
     name : "Vincenzo",
-        surname : "Pallante",
-        age : "28",
-        city : "Verona",
-        hobby : "Play the guitar",
-        favoriteFood : "Pizza",
-        favoriteVideoGame : "Call of Duty",
-        favoriteFilm : "I'm legend",
-        favoriteBook : "Undefined",
-        petName : "Marley"
+    surname : "Pallante",
+    age : "28",
+    city : "Verona",
+    hobby : "Play the guitar",
+    favoriteFood : "Pizza",
+    favoriteVideoGame : "Call of Duty",
+    favoriteFilm : "I'm legend",
+    favoriteBook : "Undefined",
+    petName : "Marley"
 }];
